f0304bc23e5a618c673ac8511a39de995adc9da9
Victor Luiz <victorluizgonzalez@gmail.com>
2025-01-21 17:25:10 -0300

    Ajustes

--- src/views/userAdmin/createupdate.vue
+++ src/views/userAdmin/createupdate.vue
@@ -18,7 +18,7 @@ const toast = useToast()
 const router = useRouter()
 const route = useRoute()
 const user = ref<IUser>({
-  status: true,
+    status: true,
 } as IUser)
 const loading = ref(false)
 const loadingSave = ref(false)
@@ -30,162 +30,163 @@ const selectProfile = ref()
 const selectClient = ref()
 
 const status = ref([
-  { label: 'ATIVO', value: true }, {
-    label: "INATIVO", value: false
-  }
+    { label: 'ATIVO', value: true }, {
+        label: "INATIVO", value: false
+    }
 ])
 
 async function getUser() {
-  loading.value = true
-  try {
-    const res = await UserServices.getUser(String(route.params.id))
-    selectClient.value = res.data.clients_users && res.data.clients_users[0]?.clients?.id_client
-    selectProfile.value = res.data.clients_users && res.data.clients_users[0]?.roles?.id_role
-    user.value = { ...res.data, status: res.data.status === 1 ? true : false }
-  } catch (error) {
-    loading.value = false
-  } finally {
-    loading.value = false
-  }
+    loading.value = true
+    try {
+        const res = await UserServices.getUser(String(route.params.id))
+        selectClient.value = res.data.clients_users && res.data.clients_users[0]?.clients?.id_client
+        selectProfile.value = res.data.clients_users && res.data.clients_users[0]?.roles?.id_role
+        user.value = { ...res.data, status: res.data.status === 1 ? true : false }
+    } catch (error) {
+        loading.value = false
+    } finally {
+        loading.value = false
+    }
 }
 
 async function getClients() {
-  try {
-    const res = await http.get('clients')
-    const resProfile = await http.get('users/list/roles')
-    perfils.value = resProfile.data.data.map((c: any) => {
-      return {
-        label: c.name,
-        value: c.id_role
-      }
-    })
-    clients.value = res.data.rows.map((c: any) => {
-      return {
-        label: c.name,
-        value: c.id_client
-      }
-    })
-  } catch (error) {
-    loading.value = false
-  } finally {
-    loading.value = false
-  }
+    try {
+        const res = await http.get('clients')
+        const resProfile = await http.get('users/list/roles')
+        perfils.value = resProfile.data.data.map((c: any) => {
+            return {
+                label: c.name,
+                value: c.id_role
+            }
+        })
+        clients.value = res.data.rows.map((c: any) => {
+            return {
+                label: c.name,
+                value: c.id_client
+            }
+        })
+    } catch (error) {
+        loading.value = false
+    } finally {
+        loading.value = false
+    }
 }
 
 onMounted(() => {
-  if (token.value) {
-    if (route.params.id) getUser()
-  }
-  getClients()
+    if (token.value) {
+        if (route.params.id) getUser()
+    }
+    getClients()
 })
 
 async function handleSubmit() {
 
-  const userPayload = {
-    name: user.value.name,
-    email: user.value.email,
-    cpf: user.value?.cpf?.replace(/\D/g, ''),
-    status: user.value.status,
-    clients: [
-      {
-        id_client: selectClient.value,
-        id_role: selectProfile.value
-      }
-    ]
-  }
-
-  console.log(selectProfile.value)
-
-  if (!selectProfile.value) {
-    toast.error('Selecione um perfil')
-    return
-  }
-
-  if (selectProfile.value !== 1 && !selectClient.value) {
-    toast.error('Selecione um cliente para o perfil selecionado')
-    return
-  }
-
-
-
-  loading.value = true
-
-  try {
-
-    if (route.params.id) {
-      await http.patch(`users/admin/${route.params.id}`, userPayload)
-      toast.success('Usuário salvo com sucesso')
-      handleBack()
-    } else {
-      const responseUser = await http.get(`users/unique?email=${userPayload.email}&document=${userPayload.cpf}`)
-      if (!responseUser.data) {
-        await http.post('users/admin', { ...userPayload })
-        toast.success('Usuário salvo com sucesso')
-        handleBack()
-      } else {
-        Swal.fire({
-          title: "Usuário já existe com esse cpf/email porém está inativado",
-          text: "Você deseja reativar?",
-          showDenyButton: false,
-          showCancelButton: true,
-          confirmButtonText: "Reativar",
-          confirmButtonColor: '#008000',
-          cancelButtonText: 'Cancelar'
-        }).then(async (result) => {
-          if (result.isConfirmed) {
-            await http.patch(`users/active/${responseUser.data.id_user}`)
-            handleBack()
-          }
-        })
-      }
-
-
+    const userPayload = {
+        name: user.value.name,
+        email: user.value.email,
+        cpf: user.value?.cpf?.replace(/\D/g, ''),
+        status: user.value.status,
+        clients: [
+            {
+                id_client: selectClient.value,
+                id_role: selectProfile.value
+            }
+        ]
     }
-  } catch (error) {
-    console.log(error)
-  } finally {
-    loading.value = false
-  }
+
+    console.log(selectProfile.value)
+
+    if (!selectProfile.value) {
+        toast.error('Selecione um perfil')
+        return
+    }
+
+    if (selectProfile.value !== 1 && !selectClient.value) {
+        toast.error('Selecione um cliente para o perfil selecionado')
+        return
+    }
+
+
+
+    loading.value = true
+
+    try {
+
+        if (route.params.id) {
+            await http.patch(`users/admin/${route.params.id}`, userPayload)
+            toast.success('Usuário salvo com sucesso')
+            handleBack()
+        } else {
+            const responseUser = await http.get(`users/unique?email=${userPayload.email}&document=${userPayload.cpf}`)
+            if (!responseUser.data) {
+                await http.post('users/admin', { ...userPayload })
+                toast.success('Usuário salvo com sucesso')
+                handleBack()
+            } else {
+                Swal.fire({
+                    title: "Usuário já existe com esse cpf/email porém está inativado",
+                    text: "Você deseja reativar?",
+                    showDenyButton: false,
+                    showCancelButton: true,
+                    confirmButtonText: "Reativar",
+                    confirmButtonColor: '#008000',
+                    cancelButtonText: 'Cancelar'
+                }).then(async (result) => {
+                    if (result.isConfirmed) {
+                        await http.patch(`users/active/${responseUser.data.id_user}`)
+                        handleBack()
+                    }
+                })
+            }
+
+
+        }
+    } catch (error) {
+        console.log(error)
+    } finally {
+        loading.value = false
+    }
 }
 function handleBack() {
-  const queryParams = router.currentRoute.value.query; // Captura as queries atuais
-  router.push({ path: '/usuarios-admin', query: queryParams }); // Retorna mantendo as queries
+    const queryParams = router.currentRoute.value.query; // Captura as queries atuais
+    router.push({ path: '/usuarios-admin', query: queryParams }); // Retorna mantendo as queries
 }
 </script>
 
 <template>
-  <div class="mt-4">
-    <FullSpinner v-if="loading" />
-    <div class="flex justify-between mb-3">
-      <h3 class="text-3xl font-medium text-gray-700">
-        {{
-          route?.fullPath === '/adicionar-usuario-admin' ? 'Novo Usuário' : 'Editar Usuário'
-        }}
-      </h3>
-      <button class="border border-gray-600 rounded px-2 text-md" @click="handleBack">
-        Voltar
-      </button>
-    </div>
-    <CardForm>
-      <template #content>
+    <div class="mt-4">
+        <FullSpinner v-if="loading" />
+        <div class="flex justify-between mb-3">
+            <h3 class="text-3xl font-medium text-gray-700">
+                {{
+                    route?.fullPath === '/adicionar-usuario-admin' ? 'Novo Usuário' : 'Editar Usuário'
+                }}
+            </h3>
+            <button class="border border-gray-600 rounded px-2 text-md" @click="handleBack">
+                Voltar
+            </button>
+        </div>
+        <CardForm>
+            <template #content>
 
-        <Form @submit="handleSubmit">
-          <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-            <base-input v-model="user.name" type="text" label="Nome Completo" rules="required|validateNameAndSurname" />
-            <Select label="Status" :options="status" v-model="user.status" />
-            <base-input v-model="user.cpf" type="text" label="CPF" rules="required|cpf" mask="###.###.###-##" />
-            <base-input v-model="user.email" type="email" label="E-mail" rules="required|email" />
-            <Select label="Perfil" :options="perfils" v-model="selectProfile" />
-            <Select v-if="selectProfile !== 1" label="Clientes" :options="clients" v-model="selectClient" />
-          </div>
-          <div class="flex justify-end mt-8">
-            <base-button type="submit" :disabled="loadingSave" :loading="loadingSave" class="max-w-fit">
-              <ISave />
-              Salvar Alterações
-            </base-button>
-          </div>
-        </Form>
-      </template>
-    </CardForm>
-  </div>
+                <Form @submit="handleSubmit">
+                    <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                        <base-input v-model="user.name" type="text" label="Nome Completo" rules="required" />
+                        <Select label="Status" :options="status" v-model="user.status" />
+                        <base-input v-model="user.cpf" type="text" label="CPF" rules="required|cpf"
+                            mask="###.###.###-##" />
+                        <base-input v-model="user.email" type="email" label="E-mail" rules="required|email" />
+                        <Select label="Perfil" :options="perfils" v-model="selectProfile" />
+                        <Select v-if="selectProfile !== 1" label="Clientes" :options="clients" v-model="selectClient" />
+                    </div>
+                    <div class="flex justify-end mt-8">
+                        <base-button type="submit" :disabled="loadingSave" :loading="loadingSave" class="max-w-fit">
+                            <ISave />
+                            Salvar Alterações
+                        </base-button>
+                    </div>
+                </Form>
+            </template>
+        </CardForm>
+    </div>
 </template>
